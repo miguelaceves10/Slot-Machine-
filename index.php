825676bd0f8e535feab56a13b509c7d71d266c95
Miguel A Aceves <maceves@csumb.edu>
2018-02-12 04:43:30 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php
+    include 'inc/functions.php';
+?>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+       
+        <style type="text/css">
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <div id= "main">
+            <?php
+       
+            
+            play();
+        
+            ?>
+            <form>
+                <input type = "submit" value ="Spin"/>
+            </form>
+        </div>
+        
+    </body>
+</html>
